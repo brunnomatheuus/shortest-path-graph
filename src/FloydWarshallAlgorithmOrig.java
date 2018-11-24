a244f606c6dfabcd313e8dc3da132b16f5d2c71e
Dionisio <dionisio@naracosta.com.br>
2018-11-24 15:48:23 -0300

    Renomeações FloydWarshall

--- src/FloydWarshallAlgorithmOrig.java
+++ src/FloydWarshallAlgorithmOrig.java
@@ -1,16 +1,14 @@
 
 // A Java program for Floyd Warshall All Pairs Shortest 
 // Path algorithm. 
-//import java.util.*;
-//import java.lang.*;
-//import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-class FloydWarshallAlgorithm9x9 {
-    final static int INF = Integer.MAX_VALUE;
-    // final static int V = 9;
+class FloydWarshallAlgorithmOrig {
+    final static int INF = Integer.MAX_VALUE, V = 4;
 
     void floydWarshall(int graph[][]) {
-        final int V = graph[0].length;
         int dist[][] = new int[V][V];
         int i, j, k;
 
@@ -47,18 +45,12 @@ class FloydWarshallAlgorithm9x9 {
         }
 
         // Print the shortest distance matrix
-        printSolution(dist, V);
+        printSolution(dist);
     }
 
-    void printSolution(int dist[][], int V) {
-        System.out.println("A seguinte matrix exibe o caminho mais curto e distâncias entre cada par de vértices:");
-        System.out.print("X\t");
+    void printSolution(int dist[][]) {
+        System.out.println("The following matrix shows the shortest " + "distances between every pair of vertices");
         for (int i = 0; i < V; ++i) {
-            System.out.print(i + "\t");
-        }
-        System.out.print("\n");
-        for (int i = 0; i < V; ++i) {
-            System.out.print(i + "\t");
             for (int j = 0; j < V; ++j) {
                 if (dist[i][j] == INF)
                     System.out.print("INF\t");
@@ -71,18 +63,16 @@ class FloydWarshallAlgorithm9x9 {
 
     // Driver program to test above function
     public static void main(String[] args) {
+        /*
+         * Let us create the following weighted graph 10 (0)------->(3) | /|\ 5 | | | |
+         * 1 \|/ | (1)------->(2) 3
+         */
         int graph[][] = { // DEFINE O GRAFO
-                { 0, 2, INF, INF, 3, INF, INF, INF, INF }, // LINHA 0
-                { 2, 0, 4, 1, INF, INF, INF, INF, INF }, // LINHA 1
-                { INF, 4, 0, INF, INF, INF, INF, INF, 5 }, // LINHA 2
-                { INF, 1, INF, 0, INF, 3, INF, 1, INF }, // LINHA 3
-                { 3, INF, INF, INF, 0, 2, INF, INF, INF }, // LINHA 4
-                { INF, INF, INF, 3, 2, 0, 1, INF, INF }, // LINHA 5
-                { INF, INF, INF, INF, INF, 1, 0, INF, 2 }, // LINHA 6
-                { INF, INF, INF, 1, INF, INF, INF, 0, 3 }, // LINHA 7
-                { INF, INF, 5, INF, INF, INF, 2, 3, 0 } }; // LINHA 8
-
-        FloydWarshallAlgorithm9x9 a = new FloydWarshallAlgorithm9x9();
+                { 0, 5, INF, 10 }, // LINHA 0
+                { INF, 0, 3, INF }, // LINHA 1
+                { INF, INF, 0, 1 }, // LINHA 2
+                { INF, INF, INF, 0 } };// LINHA 3
+        FloydWarshallAlgorithmOrig a = new FloydWarshallAlgorithmOrig();
 
         // Print the solution
         a.floydWarshall(graph);
